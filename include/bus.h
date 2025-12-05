2fab0eec3aafa6e95c9449dbe18a9c3f012ae945
Gimerbone <gimerbone@gmail.com>
2025-12-05 13:53:47 +0700

    Adding delete functions to bus

--- include/bus.h
+++ include/bus.h
@@ -25,3 +25,10 @@ busAddress createElmBus(int idBus, int kapasitas, std::string rute);
 void insertFirstBus(BusList  &L, busAddress bus);
 void insertLastBus(BusList &L, busAddress bus);
 void insertAfterBus(BusList &L, busAddress bus, busAddress prec);
+
+void deleteFirstBus(BusList  &L);
+void deleteFirstBus(BusList &L, busAddress &deleted);
+void deleteLastBus(BusList &L);
+void deleteLastBus(BusList &L, busAddress &deleted);
+void deleteAfterBus(BusList &L, busAddress prec);
+void deleteAfterBus(BusList &L, busAddress prec, busAddress &deleted);
---
+++ src/bus_103012400078.cpp
@@ -0,0 +1,75 @@
+#include "bus.h"
+
+void deleteFirstBus(BusList  &L) {
+    if (L.first == nullptr) return;
+
+    busAddress p = L.first;
+    L.first = p->next;
+    delete p;
+}
+
+void deleteFirstBus(BusList  &L, busAddress &deleted) {
+    if (L.first == nullptr) {
+        deleted = nullptr;
+        return;
+    }
+
+    deleted = L.first;
+    L.first = deleted->next;
+    deleted->next = nullptr;
+}
+
+void deleteLastBus(BusList &L) {
+    if (L.first == nullptr) {
+        return;
+    }
+
+    if (L.first->next == nullptr) {
+        delete L.first;
+        L.first = nullptr;
+    }
+    else {
+        busAddress p = L.first;
+        while (p->next->next) p = p->next;
+        delete p->next;
+        p->next = nullptr;
+    }
+}
+
+void deleteLastBus(BusList &L, busAddress &deleted) {
+    if (L.first == nullptr) {
+        deleted = nullptr;
+        return;
+    }
+
+    if (L.first->next == nullptr) {
+        deleted = L.first;
+        L.first = nullptr;
+    }
+    else {
+        busAddress p = L.first;
+        while (p->next->next) p = p->next;
+        deleted = p->next;
+        p->next = nullptr;
+    }
+}
+
+void deleteAfterBus(BusList &L, busAddress prec) {
+    if (prec == nullptr || prec->next == nullptr) {
+        return;
+    }
+
+    busAddress p = prec->next;
+    prec->next = prec->next->next;
+    delete p;
+}
+
+void deleteAfterBus(BusList &L, busAddress prec, busAddress &deleted) {
+    if (prec == nullptr || prec->next == nullptr) {
+        return;
+    }
+
+    deleted = prec->next;
+    prec->next = prec->next->next;
+    deleted->next = nullptr;
+}
